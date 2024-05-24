640c1173f2e68d370edae38915a6b4d5343988f2
bigshans <26884666+bigshans@users.noreply.github.com>
2024-05-24 22:07:16 +0800

    perf: change by myself

--- user.js
+++ user.js
@@ -10,14 +10,14 @@ user_pref("ultima.xstyle.squared", false);
 user_pref("ultima.xstyle.containertabs.i", false);
 user_pref("ultima.xstyle.containertabs.ii", false);
 user_pref("ultima.xstyle.containertabs.iii", true);
-user_pref("ultima.xstyle.closetabsbutton", false);
+user_pref("ultima.xstyle.closetabsbutton", true);
 
 user_pref("ultima.theme.extensions", true);
 
 user_pref("ultima.OS.kde", true);
 user_pref("ultima.OS.gnome", false);
 user_pref("ultima.OS.mac", false);
-user_pref("ultima.OS.linux", false);
+user_pref("ultima.OS.linux", true);
 
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("svg.context-properties.content.enabled", true);
